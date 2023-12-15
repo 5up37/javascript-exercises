bbd74475abbffe9a1af833e0c78689b77babc031
5up37 <augusto.silva.queiroz@gmail.com>
2023-12-15 19:03:48 -0300

    Solved 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let newString = ''
+let stringPart
+    while (string.length > 0) {
+        stringPart = string.slice(string.length - 1)
+        newString += stringPart
+        string = string.slice(0,string.length - 1)
+    }
+return newString
 };
 
 // Do not edit below this line
